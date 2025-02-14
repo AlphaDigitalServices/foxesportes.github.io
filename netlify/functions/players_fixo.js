251a21874292a4324b7609c4254cd832fc459115
Leonardo Eckert Marques <suporte@relt.com.br>
2025-02-14 15:00:17 -0300

    4.0.10

--- netlify/functions/players_fixo.js
+++ netlify/functions/players_fixo.js
@@ -1,32 +1,8 @@
 const fs = require('fs');
-const path = require('path');
 
-exports.handler = async (event, context) => {
+exports.handler = async function (event, context) {
   try {
-    // Caminho do diretório onde os arquivos JSON estão localizados dentro do Lambda
-    const directoryPath = path.join(__dirname, '../javascripts/jsons');
-
-    // Regex para encontrar o arquivo correspondente
-    const regex = /response_\d+\.json$/;
-
-    // Lendo o conteúdo do diretório
-    const files = fs.readdirSync(directoryPath);
-
-    // Encontrando o arquivo que corresponde ao padrão
-    const matchedFile = files.find(file => regex.test(file));
-
-    if (!matchedFile) {
-      return {
-        statusCode: 404,
-        body: JSON.stringify({ error: "Arquivo não encontrado" }),
-      };
-    }
-
-    // Caminho completo do arquivo encontrado
-    const filePath = path.join(directoryPath, matchedFile);
-
-    // Lendo o conteúdo do arquivo
-    const data = fs.readFileSync(filePath, 'utf8');
+    const data = fs.readFileSync('/javascripts/jsons/response_1739542371739.json', 'utf8');
     const bsData = JSON.parse(data);
 
     return {
@@ -34,7 +10,6 @@ exports.handler = async (event, context) => {
       body: JSON.stringify(bsData),
     };
   } catch (error) {
-    console.error('Erro:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Erro ao processar a requisição" }),
